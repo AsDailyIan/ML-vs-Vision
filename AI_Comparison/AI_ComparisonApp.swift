7beeb94d3bacec9cc852609f97a2dc4fe9a714e6
ianfu <ianfu.60@gmail.com>
2023-08-11 16:21:50 +0800

    Initial Commit

---
+++ AI_Comparison/AI_ComparisonApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_ComparisonApp.swift
+//  AI_Comparison
+//
+//  Created by 傅遠義 on 2023/8/11.
+//
+
+import SwiftUI
+
+@main
+struct AI_ComparisonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
